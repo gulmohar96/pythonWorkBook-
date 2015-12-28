92f9e06c681966f9bfaa4fef0e8613ae9f5d0d7c
Gulmohar <gulmohar96@gmail.com>
2015-12-29 07:16:44 +1100

    Study Drills and excercises 11 to 15

---
+++ ex13.1.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+fourth = input("How old are you? ")
+
+print("The scriptttttt is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("forth variable is:",fourth)
+
+
